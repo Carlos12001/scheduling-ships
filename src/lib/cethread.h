75e19c18a25ca16de6526ee11191a9ff7e4b1beb
Carlos12001 <carlos.andres12001@gmail.com>
2024-10-04 13:59:05 -0600

    docs: change name

--- src/lib/cethread.h
+++ src/lib/cethread.h
@@ -8,20 +8,21 @@
 #endif
 
 typedef struct {
-  pid_t tid;    // ID del hilo
-  void *stack;  // Puntero a la pila del hilo
+  pid_t tid;    // id thread
+  void *stack;  // pointer to stack
 } cethread_t;
 
 typedef struct {
-  volatile int lock;  // Variable de bloqueo simple
+  volatile int lock;  // 0 = unlocked, 1 = locked
 } cemutex;
 
-// Funciones CEThreads que envuelven las de pthread
+// Context from Function src/lib/cethread.c
 int cethread_create(cethread_t *thread, void *(*start_routine)(void *),
                     void *arg);
 int cethread_join(cethread_t thread, void **retval);
 int cemutex_init(cemutex *mutex);
 int cemutex_destroy(cemutex *mutex);
+int cemutex_lock(cemutex *mutex);
 int cemutex_unlock(cemutex *mutex);
 
 #endif  // CETHREAD_H
